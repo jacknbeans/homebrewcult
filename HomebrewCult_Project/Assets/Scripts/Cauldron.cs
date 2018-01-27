34e85c7400100a67cfc78fba8144baf0c57f574e
BasStaffhorst <152725@edu.nhtv.nl>
2018-01-27 21:16:50 +0100

    Added cauldron Cauldron can be used to dispatch of objects in the scene

---
+++ HomebrewCult_Project/Assets/Scripts/Cauldron.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cauldron : MonoBehaviour {
+
+    private GameObject hitObject;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void OnCollisionEnter(Collision collision)
+    {
+        hitObject = collision.gameObject;
+        if (hitObject.tag == "Interactable")
+        {
+            Destroy(hitObject);
+        }
+    }
+}
