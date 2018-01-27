064302a4182920c51bb28c2ecd6b5614e6bc2b13
Unknown <jack.reggin@gmail.com>
2018-01-27 01:38:50 +0100

    Camera now switchs between table and client
    There are two UI elements that will appear to allow the player to switch between looking at the table and looking at the client.

---
+++ HomebrewCult_Project/Assets/Core/LookAtBase.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Core
+{
+    public class LookAtBase : MonoBehaviour, IPointerEnterHandler
+    {
+        public Transform CameraRig;
+        public float CameraAngle;
+        public float TransitionTime = 0.3f;
+        public LookAtBase OtherLookAt;
+
+        protected bool StartTransition;
+
+        private float _velocity;
+        private float _threshold = 0.07f;
+
+        private void Update()
+        {
+            if (StartTransition)
+            {
+                var newAngle =
+                    Mathf.SmoothDamp(CameraRig.eulerAngles.x, CameraAngle, ref _velocity, TransitionTime);
+                CameraRig.rotation = Quaternion.Euler(newAngle, CameraRig.eulerAngles.y, CameraRig.eulerAngles.z);
+                if (Mathf.Abs(CameraAngle - CameraRig.eulerAngles.x) <= _threshold)
+                {
+                    StartTransition = false;
+                    OtherLookAt.Enable();
+                    gameObject.SetActive(false);
+                }
+            }
+        }
+
+        public void Enable()
+        {
+            gameObject.SetActive(true);
+        }
+
+        public void Disable()
+        {
+            gameObject.SetActive(false);
+            CameraRig.rotation = Quaternion.Euler(CameraAngle, CameraRig.eulerAngles.y, CameraRig.eulerAngles.z);
+        }
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            StartTransition = true;
+        }
+    }
+}
---
+++ HomebrewCult_Project/Assets/Core/LookAtManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Core
+{
+    public class LookAtManager : MonoBehaviour
+    {
+        public LookAtTable LookAtTableRef;
+        public LookAtClient LookAtClientRef;
+        public bool LookAtTableFirst = true;
+
+        private void Start()
+        {
+            if (LookAtTableFirst)
+            {
+                LookAtTableRef.Disable();
+                LookAtClientRef.Enable();
+            }
+            else
+            {
+                LookAtClientRef.Disable();
+                LookAtTableRef.Enable();
+            }
+        }
+    }
+}
