af6525c99d394a4ba609a9b579e73197cefaeef1
BasStaffhorst <152725@edu.nhtv.nl>
2018-01-27 23:48:44 +0100

    Runestone cauldron feeding implemented You can now feed the cauldron runestones and runestones can either be bad or good good runestones will glow and will lower the hunger value of the cauldron, bad runestones do not and will increase the hunger value.

--- HomebrewCult_Project/Assets/RuneStone.cs
+++ HomebrewCult_Project/Assets/RuneStone.cs
@@ -4,12 +4,17 @@ using UnityEngine;
 
 public class RuneStone : MonoBehaviour {
     public Sprite[] images;
-
-
+    public bool goodStone;
+    public float chanceBump = 0.5f;
 	// Use this for initialization
 	void Start () {
         GetComponent<SpriteRenderer>().sprite = images[Random.Range(0, images.Length)];
-	}
+        goodStone = (Random.value < chanceBump);
+        if (goodStone != true)
+        {
+            gameObject.GetComponent<ParticleSystem>().Stop();
+        }
+    }
 	
 	// Update is called once per frame
 	void Update () {
--- HomebrewCult_Project/Assets/Scripts/Cauldron.cs
+++ HomebrewCult_Project/Assets/Scripts/Cauldron.cs
@@ -5,20 +5,46 @@ using UnityEngine;
 public class Cauldron : MonoBehaviour {
 
     private GameObject hitObject;
+    private float hangryValue;
+    public float hangrySpeed = 1.0f;
+    public float nomValue = 40.0f;
+    public float badnomValue = 50.0f;
+    public Color cauldronColor;
+    private float maxValue = 255.0f;
+    private RuneStone fedStone;
+    private MeshRenderer _meshRend;
+
 	// Use this for initialization
 	void Start () {
-		
-	}
+        _meshRend = GetComponent<MeshRenderer>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
+        hangryValue += hangrySpeed * Time.deltaTime;
+        hangryValue /= maxValue;
+        cauldronColor.g = Mathf.Clamp(cauldronColor.g - hangryValue, 0.0f, 1.0f);
+        cauldronColor.b = Mathf.Clamp(cauldronColor.b - hangryValue, 0.0f, 1.0f);
+        _meshRend.material.color = cauldronColor;
 	}
     void OnCollisionEnter(Collision collision)
     {
         hitObject = collision.gameObject;
         if (hitObject.tag == "Interactable")
         {
+            if (hitObject.name == "RuneStone(Clone)")
+            {
+                fedStone = hitObject.GetComponent<RuneStone>();
+                if (fedStone.goodStone == true)
+                {
+                    hangryValue -= nomValue;
+                }
+                else
+                {
+                    hangryValue += badnomValue;
+                }
+
+            }
             Destroy(hitObject);
         }
     }
