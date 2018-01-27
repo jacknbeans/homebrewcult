2098c49784637ff73c29ec0021382f5b4dff9a9b
Unknown <jack.reggin@gmail.com>
2018-01-27 05:40:14 +0100

    Hand in scene now moves along 2D plane above table
    Switching the way to interact with objects in the scene. This will now function similarly to Surgeon Simulator.

---
+++ HomebrewCult_Project/Assets/Core/Interact.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Core
+{
+    public class Interact : MonoBehaviour
+    {
+        public float ScrollSpeed = 20.0f;
+        public Transform HeightPlane;
+
+        private void Update()
+        {
+            HeightPlane.position = new Vector3(HeightPlane.position.x, HeightPlane.position.y + Input.GetAxis("Mouse ScrollWheel") * Time.deltaTime * ScrollSpeed, HeightPlane.position.z);
+
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity, Physics.DefaultRaycastLayers, QueryTriggerInteraction.Collide))
+            {
+                transform.position = hit.point;
+            }
+        }
+    }
+}
--- HomebrewCult_Project/Assets/Core/Interactable.cs
+++ HomebrewCult_Project/Assets/Core/Interactable.cs
@@ -6,29 +6,14 @@ namespace Core
     {
         private void OnMouseDown()
         {
-            var ignoreSelf = LayerMask.NameToLayer("IgnoreSelf");
-            gameObject.layer = ignoreSelf;
         }
 
         private void OnMouseDrag()
         {
-            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-            string[] layers = {"Default"};
-            var layerMask = LayerMask.GetMask(layers);
-
-            RaycastHit hit;
-            if (Physics.Raycast(ray, out hit, Mathf.Infinity, layerMask))
-            {
-                Debug.DrawLine(ray.origin, hit.point, Color.magenta);
-                transform.position = hit.point;
-            }
         }
 
         private void OnMouseUp()
         {
-            var defaultLayer = LayerMask.NameToLayer("Default");
-            gameObject.layer = defaultLayer;
         }
     }
 }
