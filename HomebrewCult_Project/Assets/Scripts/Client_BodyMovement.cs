e0b19099e75a965453d59f5ad8073cabbed634a0
tessa19950 <tessalaurijssens@live.nl>
2018-01-27 08:12:20 +0100

    add client work, materials sprites scripts

---
+++ HomebrewCult_Project/Assets/Scripts/Client_BodyMovement.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Client_BodyMovement : MonoBehaviour {
+
+    public Material[] spriteArray;
+    public AnimationCurve moveCurve;
+    public Vector3 moveOffsetAmount;
+    public float moveSpeed;
+    private float moveTimer;
+    private Vector3 moveStartPos;
+
+    public bool isEye;
+    public float blinkChance;
+    private float blinkDuration;
+
+    private Renderer thisSpriteRend;
+
+	// Use this for initialization
+	void Start () {
+        thisSpriteRend = GetComponent<Renderer>();
+        thisSpriteRend.material = spriteArray[Random.Range(0, spriteArray.Length-1)];
+
+        moveStartPos = transform.position;
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //eye functions
+        if (isEye)
+            EyeSprite();
+
+        //wavey movement timer
+        moveTimer += Time.deltaTime * moveSpeed;
+        if (moveTimer > 1)
+            moveTimer = 0;
+        //wavey movement change position
+        transform.position = moveStartPos + (moveOffsetAmount * moveCurve.Evaluate(moveTimer));
+	}
+
+    void EyeSprite()
+    {
+        //blink
+        if (Random.Range(0, blinkChance) < 1)
+        {
+            thisSpriteRend.material = spriteArray[0];
+            blinkDuration = 0.2f;
+        }
+        if (blinkDuration > 0)
+            blinkDuration -= Time.deltaTime;
+        else
+            thisSpriteRend.material = spriteArray[1];
+    }
+}
